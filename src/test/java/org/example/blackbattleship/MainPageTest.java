777f59440ec10df0b1a881c0611063fedd4b7eb9
IGE-122477 <mpfea@iscte-iul.pt>
2026-05-17 16:08:17 +0100

    Testes corrigidos

--- src/test/java/org/example/blackbattleship/MainPageTest.java
+++ src/test/java/org/example/blackbattleship/MainPageTest.java
@@ -6,10 +6,8 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
+import static com.codeborne.selenide.Condition.*;
 import static org.junit.jupiter.api.Assertions.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
@@ -24,32 +22,31 @@ public class MainPageTest {
     @BeforeEach
     public void setUp() {
         open("https://www.jetbrains.com/");
+        $(".ch2-allow-all-btn").shouldBe(visible).click();
     }
 
     @Test
     public void search() {
         mainPage.searchButton.click();
 
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
+        $("[data-test-id='search-input']").sendKeys("Selenium");
+        $("[data-test-id='search-input']").click();
 
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+        $("input[data-test-id='search-input']").shouldHave(attribute("value", "Selenium"));
     }
 
     @Test
     public void toolsMenu() {
         mainPage.toolsMenu.click();
-
         $("div[data-test='main-submenu']").shouldBe(visible);
+        $("body").shouldHave(text("JETBRAINS IDEs"));
     }
 
     @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
-
-        $("#products-page").shouldBe(visible);
-
-        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+    public void navigationToAllTools() { //Teve de ser reajustada para todos os IDE's
+        mainPage.toolsMenu.click();
+        mainPage.allIdesButton.click();
+        $("body").shouldHave(text("All IDEs"));
+        assertTrue(Selenide.title().contains("IDEs"));
     }
 }
